591d117e44539219390af388cf90a8690cf425db
Pablito <pablito@uma.es>
2025-03-09 18:50:48 +0100

    Test de matricular
    Se ha añadido los test de matricular falta lo mismo que en los anteriores.

--- Practica1/src/test/java/clubdeportivo/GrupoTest.java
+++ Practica1/src/test/java/clubdeportivo/GrupoTest.java
@@ -22,19 +22,52 @@ public class GrupoTest {
         //Comprobamos
         assertEquals(grupoTest.getPlazas(),13);
     }
-
+    @Test
     public void actualizarPlazas_returnNumPlazas_MenorqueCero() throws ClubException{
         grupoTest.actualizarPlazas(-3);
         // no se que poner para que salte el error
     }
-
+    @Test
     public void actualizarPlazas_returnNumPlazas_IgualqueCero() throws ClubException{
         grupoTest.actualizarPlazas(0);
         // no se que poner para que salte el error
     }
-
+    @Test
     public void actualizarPlazas_returnNumPlazas_NumMatriculadosMayorN() throws ClubException{
-        grupoTest.actualizarPlazas(5);
+        grupoTest.actualizarPlazas(4);
+        // no se que poner para que salte el error
+    }
+
+
+
+    @Test
+    public void matricular_returnNumPlazas_MayorqueCero_MenorPlazasLibres() throws ClubException{
+        //Hacemos que se matriculen siendo menos que las plazas libres
+        grupoTest.matricular(4);
+
+        //Comprobamos
+        assertEquals(grupoTest.plazasLibres(),1);
+    }
+
+    @Test
+    public void matricular_returnNumPlazas_MayorPlazasLibres() throws ClubException{
+        //Hacemos que se matriculen siendo menos que las plazas libres
+        grupoTest.matricular(7);
+        // no se que poner para que salte el error
+    }
+
+    @Test
+    public void matricular_returnNumPlazas_MenorqueCero() throws ClubException{
+        //Hacemos que se matriculen siendo menos que las plazas libres
+        grupoTest.matricular(-3);
+
+        // no se que poner para que salte el error
+    }
+
+    @Test
+    public void matricular_returnNumPlazas_IgualqueCero() throws ClubException{
+        //Hacemos que se matriculen siendo menos que las plazas libres
+        grupoTest.matricular(0);
         // no se que poner para que salte el error
     }
 }
